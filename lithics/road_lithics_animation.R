cdf077810b1fe487c113b6f3aff9081ced2d6fae
cs <>
2019-09-09 11:07:50 +0200

    lithics update: 20 ka
    Animation of lithics ranges between 3 Ma and 20ka

--- lithics/road_lithics_animation.R
+++ lithics/road_lithics_animation.R
@@ -14,7 +14,7 @@ ROAD_all <- read_delim("CS_ROAD_ages_lithics.csv", ";", escape_double = FALSE, t
 ROAD_all$query_age_min <- as.integer(ROAD_all$query_age_min/1000) 
 ROAD_all$query_age_max <- as.integer(ROAD_all$query_age_max/1000)
 ROAD_all$range <- as.integer(ROAD_all$query_age_max-ROAD_all$query_age_min) # calculate legth of occupation
-#ROAD_all <- subset(ROAD_all, query_age_min >= 40)
+ROAD_all <- subset(ROAD_all, query_age_min >= 10)
 ROAD_all <- tidyr::drop_na(ROAD_all, c(query_age_min,query_age_max)) # remove NAs from age columns
 ROAD_all$steps <- as.integer(cut(ROAD_all$query_age_min, # categorize in 10ka-slices
                 breaks= seq(-1,3000,10), label=FALSE)*10)
@@ -34,7 +34,7 @@ plot(world)
 ### Create Map with ggplot and gganimate
 ## Linear time (first 10 seconds = 1 Ma are super boring)
 map <- world +
-  geom_point(data=ROAD_all, aes(x=locality.x, y=locality.y, group=assemblage.name, col=assemblage.name),
+  geom_point(data=ROAD_all, aes(x=locality.x, y=locality.y, group=locality.idlocality, col=locality.idlocality),
              size=2, colour = 'red', alpha = 1) +
   transition_time(time=-ROAD_all$steps) +
   #transition_states(ROAD_all$steps)+
@@ -47,7 +47,7 @@ map <- world +
   theme(plot.title = element_text(size=18))
 
 # Test animation
-animate(map, fps=5, duration=10, start_pause = 0, end_pause = 0)
+animate(map, fps=5, duration=10, start_pause = 0, end_pause = 0, renderer = gifski_renderer(loop = FALSE))
 
 # Export as gif
 anim <- animate(map, fps=30, duration=31, end_pause = 1, renderer = gifski_renderer(loop = FALSE)) # ca. 5 mins rendering
@@ -56,10 +56,10 @@ anim_save(filename="road_lithics3MaLIN_30sec_30fps.gif", animation = anim)
 
 ## Exponential
 map <- world +
-  geom_point(data=ROAD_all, aes(x=locality.x, y=locality.y, group=assemblage.name, col=assemblage.name),
+  geom_point(data=ROAD_all, aes(x=locality.x, y=locality.y, group=locality.idlocality, col=locality.idlocality),
              size=2, colour = 'red', alpha = 1) +
   transition_time(time=-ROAD_all$steps_exp) +
-  shadow_mark(past=TRUE, size=2, fill="black", colour=NA, alpha=.1) +
+  shadow_mark(past=TRUE, size=2, colour="black", alpha=.1) +
   enter_fade() +
   exit_fade() +
   labs(title = '{round(exp(-frame_time))} ka BP') +
@@ -70,6 +70,8 @@ animate(map, fps=5, duration=10, start_pause = 0, end_pause = 0)
 
 # Export as gif
 anim <- animate(map, fps=30, duration=31, end_pause = 1, renderer = gifski_renderer(loop = FALSE))
-anim_save(filename="road_lithics3MaEXP_30sec_30fps.gif", animation = anim)
+anim_save(filename="road_lithics3Ma-20ka_EXP_30sec_30fps.gif", animation = anim)
 
+anim <- animate(map, fps=30, duration=31, end_pause = 1, renderer = gifski_renderer(loop = TRUE))
+anim_save(filename="road_lithics3Ma-20ka_EXP_30sec_30fps_loop.gif", animation = anim)
      
